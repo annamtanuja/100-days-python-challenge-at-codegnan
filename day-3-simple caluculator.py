7fbd2772bfd6b8f14cf82cb227eda958b0b91dc7
A. Tanuja <tanujaannam555@gmail.com>
2026-01-08 22:52:38 +0530

    Add files via upload
    operators

---
+++ day-3-simple caluculator.py
@@ -0,0 +1,12 @@
+#simple calculator operations
+#variable and operators
+a=10
+b=20
+addition=a+b
+print("sum of two numbers is:", addition)
+print("subtraction of two numbers is:",a-b)
+print("multiplication of two numbers is:",a*b)
+print("division of two numbers is:",b/a)
+print("floor division of two numbers is:",b//a)
+print("modulo division of two numbers is:",b%a)
+print("power of two numbers is :",b**a)
