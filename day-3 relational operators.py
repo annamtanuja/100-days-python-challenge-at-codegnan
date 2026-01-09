c3b2ba775db7663fcac6a778724bf42f585ca4a3
A. Tanuja <tanujaannam555@gmail.com>
2026-01-09 13:49:55 +0530

    Add files via upload
    relational, logical, assignment , bitwise operators

---
+++ day-3 relational operators.py
@@ -0,0 +1,11 @@
+#Realtional operators
+a=20
+b=30
+print("Relational Operators")
+print("20 less than 30 is :",20<30)
+print("20 lessthan 30 is:",20<30)
+print("20 greeaterthan 30 is:",20>30)
+print("20 lessthan or equal to 30 is:",20<=30)
+print("20 greatertan or equal to 30 is:",20>=30)
+print("20 equal to equal to 30 is:",20==30)
+print("20 not equal to 30 is:",20!=30)
